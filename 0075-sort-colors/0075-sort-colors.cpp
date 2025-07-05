9906e6855baae280f6e3bb3631ebf1aaaa04f873
Saurabh Yadav <saurabh.7.ydv@gmail.com>
2025-07-05 08:04:28 +0530

    Time: 0 ms (100%), Space: 11.6 MB (78.94%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,20 +1,30 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int countZero = 0;
-        int countOne = 0;
-        int countTwo = 0;
-        for(int i=0; i< nums.size(); i++)
+        int n = nums.size();
+        int lo = 0;
+        int mid = 0; 
+        int hi = n-1;
+        /*
+        arr[mid] = 0, then swap arr[lo] and arr[mid] and increment lo by 1 because 
+        all the zeros are till index lo - 1 and move to the next element so increment 
+        mid by 1.
+        arr[mid] = 1, then move to the next element so increment mid by 1.
+        arr[mid] = 2, then swap arr[mid] and arr[hi] and decrement hi by 1 because 
+        all the twos are from index hi + 1 to n - 1. Now, we don't move to the next 
+        element because the element which is now at index mid can be a 0 and therefore 
+        needs to be checked again.
+        */
+        while(mid <= hi)
         {
-            if(nums[i] == 0) countZero++;
-            else if(nums[i] == 1) countOne++;
-            else countTwo++;
-        }
-        for(int i=0; i< nums.size(); i++)
-        {
-            if(i<countZero) nums[i] = 0;
-            else if (i<countZero + countOne) nums[i] = 1;
-            else nums[i] = 2;
+            if(nums[mid] == 0)
+            {swap(nums[mid++], nums[lo++]);}
+
+            else if(nums[mid] == 1)
+            {mid++;}
+            
+            else
+            {swap(nums[mid], nums[hi--]);}
         }
     }
 };
