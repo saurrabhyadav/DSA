fba5631574d01daeee83e5aa30ea3a29e462b9ea
Saurabh Yadav <saurabh.7.ydv@gmail.com>
2025-07-05 16:45:50 +0530

    Time: 0 ms (100%), Space: 28.2 MB (25.66%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         //Moore Voting Algorithm
+        //The algorithm works on the basis of the assumption that the majority element occurs more than n/2 times in the array. This assumption guarantees that even if the count is reset to 0 by other elements, the majority element will eventually regain the lead.
         int count = 0;
         int candidate = 0;
         
