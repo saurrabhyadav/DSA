3759aee6ff0637c77f5d97a6d3b90e463717f799
Saurabh Yadav <saurabh.7.ydv@gmail.com>
2025-07-29 09:22:45 +0530

    Time: 0 ms (100%), Space: 26.1 MB (91.72%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int low = 0, high = nums.size() - 1;
+        int low = 0, high = nums.size() - 1, mid, first_occurence, second_occurence;
 
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            // Check for single element
-            bool leftSame = (mid > 0 && nums[mid] == nums[mid - 1]);
-            bool rightSame = (mid < nums.size() - 1 && nums[mid] == nums[mid + 1]);
-
-            if (!leftSame && !rightSame) {
-                return nums[mid];
+        while(low <= high)
+        {
+            mid = low + (high - low)/2;
+            if(mid>0 && nums[mid] == nums[mid-1]) 
+            {
+                first_occurence = mid - 1;
+                second_occurence = mid;
+                //if first_occurence at even index implies single element is in
+                //right half
+                if(first_occurence % 2 == 0) low = mid + 1;
+                else high = mid - 2;
             }
-            else if (leftSame) {
-                int first = mid - 1;
-                if (first % 2 == 0)
-                    low = mid + 1;
-                else
-                    high = mid - 2;
+            else if(mid < nums.size()-1 && nums[mid] == nums[mid+1]) 
+            {
+                second_occurence = mid + 1;
+                first_occurence = mid;
+                //if second_occurence at odd index implies single element is in
+                //left half
+                if(second_occurence % 2 != 0) low = mid + 2;
+                else high = mid - 1;
             }
-            else { // rightSame
-                int second = mid + 1;
-                if (second % 2 != 0)
-                    low = mid + 2;
-                else
-                    high = mid - 1;
-            }
+            //number at mid occurs only 1 time
+            else return nums[mid];
         }
-        return -1; 
+        return -1;
     }
 };
